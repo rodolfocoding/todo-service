cb9f5a724e2d6f6fe5c689f6d6322dcffabc4c9b
rodolfo.silva@bilheteriadigital.com <rodolfo.coding@gmail.com>
2023-05-27 02:26:51 -0300

    feat: implementing list handler

---
+++ handlers/list.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/rodolfocoding/todo-service/models"
+)
+
+func List(w http.ResponseWriter, r *http.Request) {
+
+	todos, err := models.GetAll()
+
+	if err != nil {
+		log.Printf("Erro ao obter registros: %v", err)
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(todos)
+}
