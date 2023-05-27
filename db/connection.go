8925524cad4217d1a397dc7ac7b561a926fc7993
rodolfo.silva@bilheteriadigital.com <rodolfo.coding@gmail.com>
2023-05-27 01:51:51 -0300

    chore: config database

---
+++ db/connection.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+	"github.com/rodolfocoding/todo-service/configs"
+)
+
+func OpenConnection() (*sql.DB, error) {
+	conf := configs.GetDB()
+
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s pass=%s dbname=%s sslmode=disable", conf.Host, conf.Port, conf.User, conf.Pass, conf.Name)
+
+	conn, err := sql.Open("postgres", connectionString)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = conn.Ping()
+
+	return conn, err
+}
