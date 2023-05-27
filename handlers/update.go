94b53f2bb211f25e0a1af70492da8f9cc5c9aa95
rodolfo.silva@bilheteriadigital.com <rodolfo.coding@gmail.com>
2023-05-27 02:20:06 -0300

    feat: implementing update handle

---
+++ handlers/update.go
@@ -0,0 +1,53 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi"
+	"github.com/rodolfocoding/todo-service/models"
+)
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+
+	if err != nil {
+		log.Printf("Erro ao fazer parse do id: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	var todo models.Todo
+
+	err = json.NewDecoder(r.Body).Decode(&todo)
+
+	if err != nil {
+		log.Printf("Erro ao fazer decode do json: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	rows, err := models.Update(int64(id), todo)
+
+	if err != nil {
+		log.Printf("Erro ao atualizar resgistro: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	if rows > 1 {
+		log.Printf("Error: foram atualizados %d registros", rows)
+	}
+
+	resp := map[string]any{
+		"Error":   true,
+		"Message": "Dados atulizados com sucesso",
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(resp)
+
+}
